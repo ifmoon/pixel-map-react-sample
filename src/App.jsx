374a98e00fe1c40479f486ec518624e18271f514
ifmoon <ifmoon.io@gmail.com>
2020-12-18 19:11:13 +0900

    ㅡ

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,26 @@ import maps from './maps';
 
 const appStyle = {
   display: 'grid',
-  gridAutoRows: '1fr',
-  gridAutoColumns: '1fr',
-  alignItems: 'center',
-  justifyItems: 'center',
+  gridTemplateRows: 'repeat(52, 5px)',
+  gridTemplateColumns: 'repeat(78, 5px)',
   width: '80vh',
-  height: '50vh'
+  height: '50vh',
+  gap: '1px',
+  justifyItems: 'center',
+  aliginItems: 'center'
 }
 
 const getRandomHexCode = () => `#${Math.floor(Math.random()*16777215).toString(16)}`;
 
 const Cell = ({name, x, y, color}) => {
-  const cellStyle = useMemo(()=>({gridColumn: `${x}/${x}`, gridRow: `${y}/${y}`, width: '80%', height: '80%', backgroundColor: color, borderRadius: '50%'}),[x,y, color]);
+  const cellStyle = useMemo(()=>({gridColumn: `${x}/${x}`, gridRow: `${y}/${y}`, width: '85%', height: '85%', backgroundColor: color, borderRadius: '50%' }),[x,y, color]);
   return <div id={name} style={cellStyle} />
 }
 
 function App() {
 
   return (
-      <div style={appStyle}>
+      <div style={appStyle} id="map">
         {maps.map(({name, locations }) => {
           const color = getRandomHexCode();
           return <>{locations.map(({x,y}) => (<Cell key={`${name}_${x}_${y}`} name={name} x={x} y={y} color={color}/>))}</>
